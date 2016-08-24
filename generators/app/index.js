b42c6034dc9033c115732d9f3b7c9bfcc799971d
Guillermo de la Iglesia <zguillez@gmail.com>
2016-08-24 15:25:13 +0200

    fix npm install

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,33 +3,21 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 module.exports = yeoman.Base.extend({
-	initializing: function() {
+	initializing: function () {
 		this.pkg = require('../../package.json');
-	},
-	prompting: function() {
-		var done = this.async();
 		this.log(yosay('Welcome to the zetadelic ' + chalk.red('AngularJS v' + this.pkg.version) + ' generator!'));
-		var prompts = [];
-		this.prompt(prompts, function(props) {
-			this.props = props;
-			done();
-		}.bind(this));
 	},
-	writing: {
-		app: function() {
-			this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
-			this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
-			this.fs.copy(this.templatePath('Gruntfile.js'), this.destinationPath('Gruntfile.js'));
-			this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
-			this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
-		},
-		projectfiles: function() {
-			this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
-			this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
-			this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
-		}
+	writing: function () {
+		this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
+		this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
+		this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
+		this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
+		this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
+		this.fs.copy(this.templatePath('Gruntfile.js'), this.destinationPath('Gruntfile.js'));
+		this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
+		this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
 	},
-	install: function() {
+	install: function () {
 		this.installDependencies();
 	}
 });
